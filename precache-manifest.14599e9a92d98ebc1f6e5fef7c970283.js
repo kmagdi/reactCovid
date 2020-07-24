06f096d68040b243510cc0d50e0f0337b5f9d08d
kmagdi <katay.magdi@gmail.com>
2020-07-24 16:33:32 +0200

    Updates

---
+++ precache-manifest.14599e9a92d98ebc1f6e5fef7c970283.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f971c00d60340a08e0b8bca869ea2ce",
+    "url": "/reactCovid/index.html"
+  },
+  {
+    "revision": "85edbfd711b04283098d",
+    "url": "/reactCovid/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "aa7a07f600fd07fba894",
+    "url": "/reactCovid/static/css/main.be9475d0.chunk.css"
+  },
+  {
+    "revision": "85edbfd711b04283098d",
+    "url": "/reactCovid/static/js/2.4ff717ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactCovid/static/js/2.4ff717ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa7a07f600fd07fba894",
+    "url": "/reactCovid/static/js/main.d43e15a6.chunk.js"
+  },
+  {
+    "revision": "a38465204bf11690f245",
+    "url": "/reactCovid/static/js/runtime-main.61521117.js"
+  },
+  {
+    "revision": "8595bdcde8afcd2f3fb4ca50365bcb11",
+    "url": "/reactCovid/static/media/population.8595bdcd.txt"
+  }
+]);
