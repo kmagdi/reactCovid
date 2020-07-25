7f7bb1854de7c665773e63b1728e4e6b6bfb173a
kmagdi <katay.magdi@gmail.com>
2020-07-25 17:10:45 +0200

    design navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,17 +7,23 @@ class NavBar extends Component {
     render() { 
         return (
             <nav className="navbar navbar-dark bg-dark">
-                <img style={{width:"30px", height:"30px"}}  src={covid} alt="icon"/>
+                <div className="col-sm-1">
+                    <img style={{width:"30px", height:"30px"}}  src={covid} alt="icon"/>
+                </div>
+                
                 
                 <div className="col-sm-3">
                     <span className="navbar-brand mb-0 ">Select country:</span > 
-                    <select  value={this.props.selectedOption} onChange={(e)=>this.props.onSelected(e.target.value)}>
+                    <select value={this.props.selectedOption} onChange={(e)=>this.props.onSelected(e.target.value)}>
                         <option>no selected</option>
                         {this.props.data.map((obj) => <option key={obj.country}>{obj.country}</option>)}
                     </select>
                 </div>
-                <h1 className="navbar-brand mb-0 ">COVID-19</h1>
-                <h3 className="navbar-brand mb-0 ">{this.props.text}  </h3>      
+                <div className="col-sm-8 text-center">
+                    <h1 className="navbar-brand mb-0 ">COVID-19</h1>
+                    <h3 className="navbar-brand mb-0 ">{this.props.text}  </h3>   
+                </div>
+                   
             </nav>
           );
     }
